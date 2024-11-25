9f92a85ce33685f3e650a21e420861d57050cfaf
Poornika Bonam <97566249+poornikabonam@users.noreply.github.com>
2024-11-25 09:00:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,8 @@ ingredients_list= st.multiselect('Choose up to 5 ingredients:',my_dataframe)
 if ingredients_list:
     ingredients_string=''
 
-    for fruit_choden in ingredients_list:
-        ingredients_string+=fruit_choden + ' '
+    for fruit_chosen in ingredients_list:
+        ingredients_string+=fruit_chosen + ' '
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
